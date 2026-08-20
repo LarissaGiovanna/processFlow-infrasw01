cf921a736089322d9fff376a583c31a77f09e1f9
LarissaGiovanna <lgiova544@gmail.com>
2026-08-20 18:12:41 -0300

    feat: tratamento das entradas stdin

--- processFlow.c
+++ processFlow.c
@@ -3,56 +3,75 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+void task(char *nameTask)
+{
+}
 int main(int argc, char const *argv[])
 {
-//inicializacao modos interativo e workflow
-if (argc > 1){ //workflow
-    printf("%s\n", argv[1]);
-    FILE *file = fopen(argv[1], "r");
-    if (file == NULL) {
-        printf("Arquivo nao encontrado. Verifique o caminho e tente novamente.\n");
-        return 1;
-    }else{
-        char command[256];
-        fgets(command, sizeof(command), file);
-    
-        //strtok para separar os comandos
-        char *token;
-        char *rest = command;
-        while ((token = strtok_r(rest, " ", &rest))) {
-            printf("comando encontrado: %s\n", token);
-            //executar os comandos
+    // inicializacao modos interativo e workflow
+    if (argc > 1)
+    { // workflow
+        printf("%s\n", argv[1]);
+        FILE *file = fopen(argv[1], "r");
+        if (file == NULL)
+        {
+            printf("Arquivo nao encontrado. Verifique o caminho e tente novamente.\n");
+            return 1;
         }
-    fclose(file);
-    return 0;
-    }
-}else{
-    char command[256] = "";
-    while (strcmp(command, "exit") != 0) {
-        printf("processflow> ");
-        scanf("%s", command);
-        printf("comando encontrado: %s\n", command);
+        else
+        {
+            char command[256];
+            fgets(command, sizeof(command), file);
 
-        if (strcmp(command, "exit") == 0 || feof(stdin)) { //saida exit
-            printf("Saindo do programa.\n");
-            break;
+            // strtok para separar os comandos
+            char *token;
+            char *rest = command;
+            while ((token = strtok_r(rest, " ", &rest)))
+            {
+                printf("comando encontrado: %s\n", token);
+                // executar os comandos
+            }
+            fclose(file);
+            return 0;
         }
     }
+    else
+    { // interativo
+        char command[256] = "";
+        while (strcmp(command, "exit") != 0)
+        {
+            printf("processflow> ");
+            fgets(command, sizeof(command), stdin);
+            command[strcspn(command, "\n")] = '\0'; // remove \n do final da string
+            printf("comando encontrado: %s\n", command);
+            printf("tamanho do comando: %ld\n", strlen(command));
 
+            printf("tam string comparacao: %ld\n", strlen("task"));
+            printf("comp: %d\n", strcmp(command, "task"));
+
+            if (strstr(command, "task") != NULL && strstr(command, "task") == &command[0]) //verifica se na string digitada existe a palavra task e se task é a primeira palavra da string verificando o endereço de memoria do inicio da string
+            { // task
+                printf("OK");
+            }
+
+            if (strcmp(command, "exit") == 0 || feof(stdin))
+            { // saida exit
+                printf("Saindo do programa.\n");
+                break;
+            }
+        }
+    }
 }
 
-}
-
-
-//processos
-    // pid_t pid = fork();
-    // if (pid == 0) {
-    //     // Child process
-    //     printf("Child process: %d\n", getpid());
-    // } else if (pid > 0) {
-    //     // Parent process
-    //     printf("Parent process: %d\n", getpid());
-    // } else {
-    //     // Fork failed
-    //     perror("fork");
-    // }
+// processos
+//  pid_t pid = fork();
+//  if (pid == 0) {
+//      // Child process
+//      printf("Child process: %d\n", getpid());
+//  } else if (pid > 0) {
+//      // Parent process
+//      printf("Parent process: %d\n", getpid());
+//  } else {
+//      // Fork failed
+//      perror("fork");
+//  }
